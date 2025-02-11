714d3c6700490bdf2851676d41a44b86d008f278
tort <knatashmbler.18@gmail.com>
2025-02-12 02:46:46 +0700

    add competed and data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+Vue.component("plan-work", {
+    template: `
+    
+    `,
+
+    methods:{
+
+    },
+
+    data(){
+        return {
+            title: null,
+            description: null,
+            deadline: null,
+            titleEdited: this.title,
+            descriptionEdited: this.description,
+            deadlineEdited: this.deadline,
+            isEdit: false,
+            isReturn: false,
+            reason: null,
+            date: new Date(),
+            isTime: 'Task complete in time',
+            reasons: [],
+            card: [],
+            errors: [],
+            plans: [],
+            works: [],
+            tests: [],
+            complete: []
+        }
+    },
+
+    computed:{
+        time(){
+            return formatter.format(this.date)
+        },
+
+        beforeMount(){
+            this.works = JSON.parse(localStorage.getItem('works'))
+            this.reasons = JSON.parse(localStorage.getItem('reasons'))
+            this.tests = JSON.parse(localStorage.getItem('tests'))
+            this.complete = JSON.parse(localStorage.getItem('complete'))
+        }
+    }
+})
+
 let app = new Vue({
     el: '#app',
     data: {
